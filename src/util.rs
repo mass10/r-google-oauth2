993b270ceeea1c3fdac4ce4094b11c6353a821a2
Irisawa Masaru <irisawa.masaru@gmail.com>
2025-03-01 15:31:01 +0900

    Update util.rs

--- src/util.rs
+++ src/util.rs
@@ -4,7 +4,9 @@
 //! # References
 //! - [モバイル &デスクトップ アプリ向け OAuth 2.0](https://developers.google.com/identity/protocols/oauth2/native-app?hl=ja)
 
+///
 /// 現在のタイムスタンプを取得します。
+///
 pub fn get_current_timestamp() -> String {
 	let now = chrono::Local::now();
 	let timestamp = now.format("%Y-%m-%d %H:%M:%S%.3f").to_string();
@@ -33,6 +35,9 @@ macro_rules! error {
     };
 }
 
+///
+/// URL クエリストリングを解析します。
+///
 fn split_querystring(url: &str) -> std::collections::HashMap<String, String> {
 	if !url.contains("?") {
 		return std::collections::HashMap::new();
@@ -49,7 +54,9 @@ fn split_querystring(url: &str) -> std::collections::HashMap<String, String> {
 	return query;
 }
 
+///
 /// "GET /?state=... HTTP/1.1" から URL を抽出します。
+///
 fn get_request_path(unknown: &str) -> String {
 	if !unknown.starts_with("GET /") {
 		return String::new();
@@ -59,7 +66,9 @@ fn get_request_path(unknown: &str) -> String {
 	return url.to_string();
 }
 
+///
 /// HTTP リクエストを解析します。
+///
 pub fn diagnose_http_request(http_request: &Vec<String>) -> std::collections::HashMap<String, String> {
 	for line in http_request {
 		let url = get_request_path(line);
@@ -71,6 +80,9 @@ pub fn diagnose_http_request(http_request: &Vec<String>) -> std::collections::Ha
 	return std::collections::HashMap::new();
 }
 
+///
+/// URL デコード
+///
 fn urldecode(s: &str) -> String {
 	let mut result = String::new();
 	let mut i = 0;
@@ -89,7 +101,9 @@ fn urldecode(s: &str) -> String {
 	result
 }
 
+///
 /// 使用可能な TCP ポートをランダムに選択します。
+///
 pub fn select_random_tcp_port() -> Result<u16, Box<dyn std::error::Error>> {
 	for port in 15000..29000 {
 		if try_bind_tcp_port(port)? {
@@ -100,7 +114,9 @@ pub fn select_random_tcp_port() -> Result<u16, Box<dyn std::error::Error>> {
 	return Err("No port available".into());
 }
 
+///
 /// TCP ポートが使用可能かどうかを確認します。
+///
 fn try_bind_tcp_port(port: u16) -> Result<bool, Box<dyn std::error::Error>> {
 	let address = format!("127.0.0.1:{}", port);
 	let result = std::net::TcpListener::bind(&address);
@@ -110,14 +126,18 @@ fn try_bind_tcp_port(port: u16) -> Result<bool, Box<dyn std::error::Error>> {
 	return Ok(true);
 }
 
+///
 /// ブラウザーを開きます。
+///
 pub fn open_browser(url: &str) -> Result<(), Box<dyn std::error::Error>> {
 	info!("OPEN> {}", url);
 	open::that(url)?;
 	return Ok(());
 }
 
+///
 /// URL エンコーディング
+///
 pub fn urlencode(s: &str) -> String {
 	let mut result = String::new();
 	for c in s.chars() {
@@ -130,7 +150,9 @@ pub fn urlencode(s: &str) -> String {
 	return result;
 }
 
+///
 /// QueryString を作成します。
+///
 #[allow(unused)]
 fn build_query_string(params: &std::collections::HashMap<&str, &str>) -> String {
 	let mut query = String::new();
@@ -149,7 +171,9 @@ fn build_query_string(params: &std::collections::HashMap<&str, &str>) -> String 
 
 // https://oauth2.googleapis.com/tokeninfo
 
+///
 /// BASE64 文字列の特別な変換
+///
 pub fn fix_base64_string(s: &str) -> String {
 	let s = s.replace("=", "");
 	let s = s.replace("+", "-");
@@ -157,14 +181,18 @@ pub fn fix_base64_string(s: &str) -> String {
 	return s;
 }
 
+///
 /// コード検証ツールとしての文字列を生成します。
+///
 pub fn generate_random_string(size: u32) -> String {
 	let buffer = generate_random_u8_array(size);
 	let s = encode_base64(&buffer);
 	return fix_base64_string(&s);
 }
 
+///
 /// SHA256 ハッシュ
+///
 pub fn create_sha256b_hash(s: &str) -> Vec<u8> {
 	use sha2::Digest;
 
@@ -175,14 +203,18 @@ pub fn create_sha256b_hash(s: &str) -> Vec<u8> {
 	return result.as_slice().to_vec();
 }
 
+///
 /// code_verifier >> code_challenge
+///
 pub fn generate_code_challenge(s: &str) -> String {
 	let buffer = create_sha256b_hash(s);
 	let s = encode_base64(&buffer);
 	return fix_base64_string(&s);
 }
 
+///
 /// BASE64 エンコーディング
+///
 pub fn encode_base64(buffer: &[u8]) -> String {
 	use base64::Engine;
 
@@ -190,7 +222,9 @@ pub fn encode_base64(buffer: &[u8]) -> String {
 	return result;
 }
 
+///
 /// ランダムな u8 バイト配列を生成します。
+///
 pub fn generate_random_u8_array(length: u32) -> Vec<u8> {
 	use rand::Rng;
 	let mut result: Vec<u8> = vec![];
@@ -201,6 +235,9 @@ pub fn generate_random_u8_array(length: u32) -> Vec<u8> {
 	return result;
 }
 
+///
+/// 
+///
 pub trait MapHelper {
 	fn get_string(&self, key: &str) -> String;
 }
@@ -218,7 +255,9 @@ impl MapHelper for std::collections::HashMap<String, String> {
 	}
 }
 
+///
 /// 簡易的なストップウォッチ
+///
 pub struct SimpleStopWatch {
 	start: std::time::Instant,
 }
@@ -236,6 +275,9 @@ impl SimpleStopWatch {
 	}
 }
 
+///
+/// POST リクエストを送信します。(フォーム送信型)
+///
 pub fn http_post(url: &str, params: &std::collections::HashMap<&str, &str>) -> Result<String, Box<dyn std::error::Error>> {
 	let client = reqwest::blocking::Client::new();
 	let response = client.post(url).form(params).send()?;
@@ -243,6 +285,9 @@ pub fn http_post(url: &str, params: &std::collections::HashMap<&str, &str>) -> R
 	return Ok(text);
 }
 
+///
+/// GET リクエストを送信します。
+///
 pub fn http_get(url: &str) -> Result<String, Box<dyn std::error::Error>> {
 	let client = reqwest::blocking::Client::new();
 	let response = client.get(url).send()?;
